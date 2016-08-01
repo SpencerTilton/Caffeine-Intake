d2916fa86ccd137435ba2a7a442212f135e39aec
SpencerTilton <tiltospe@hotmail.com>
2016-07-31 21:36:53 -0700

    FirstWorkingBuild

--- index.js
+++ index.js
@@ -12,9 +12,16 @@ function checkDate() {
 var caffeine = {
     addCup: function () {
         caffeineInSystem  += 100;
+        this.viewCaffeine();
+        this.interval = setInterval(reduceCaffeine, 1000);
     },
 
     viewCaffeine: function () {
         document.getElementById("output").innerHTML = caffeineInSystem;
     },
+}
+
+function reduceCaffeine() {
+    caffeineInSystem = caffeineInSystem * Math.pow(.5, 4.87e-5);
+    caffeine.viewCaffeine();
 }
